5d37c7b677fee77240eef925b34c8ca78c18d9d5
Ian Lancaster <ianclancaster@gmail.com>
2017-02-23 10:49:33 -0700

    Add form HTML"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.submitGrudge}>
+          <h2>Add Grudge</h2>
+          <label for="name">Name of person who offended you:</label>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            required
+          />
+          <label for="offense">What did they do?</label>
+          <input
+            type="offense"
+            placeholder="Sneezed during my presentation"
+            name="offense"
+            required
+          />
+          <label for="date">When did the offense occur?</label>
+          <input
+            type="date"
+            name="date"
+            required
+          />
+          <button type="submit">Remember Forever</button>
+        </form>
       </div>
     );
   }
